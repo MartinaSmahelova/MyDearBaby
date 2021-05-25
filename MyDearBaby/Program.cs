8499a05a2c80f32069b1ed032d18abc7142c1c86
Martina Šmahelová <mata.smahelova@gmail.com>
2021-05-25 19:57:09 +0200

    try catch exceptions handling program.cs

--- MyDearBaby/Program.cs
+++ MyDearBaby/Program.cs
@@ -1,20 +1,31 @@
 using System;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace MyDearBaby
 {
     static class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MyDearChildMainForm());
+            try
+            {
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MyDearChildMainForm());
+            }
+            catch (Exception ex)
+            {
+                log.Error($"There are some problems in main method.", ex);
+            }
+   
         }
     }
 }
